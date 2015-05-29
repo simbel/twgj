2518683b01a74c44ee6faeef0c3d5492713c6886
dliu2 <dliu2@tek.com>
2015-05-29 14:51:54 +0800

    add source code until chapter 8, refactor build.xml

---
+++ src/twgj/ch8/MovieTitles.java
@@ -0,0 +1,19 @@
+package twgj.ch8;
+
+import java.util.*;
+
+/**
+ * Code for listing 8_7
+ */
+public class MovieTitles {
+  public static void main(String[] args) {
+    List<String> movieTitles = new ArrayList<>();
+    movieTitles.add("Seven");
+    movieTitles.add("Snow White");
+    movieTitles.add("Die Hard");
+
+    for (String movieTitle : movieTitles) {
+      System.out.println(movieTitle);
+    }
+  }
+}
